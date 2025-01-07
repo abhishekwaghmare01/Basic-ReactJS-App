7d1ee20793b835766944f09238e429d1a5e0487e
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-01-07 11:18:35 +0530

    Handling click event

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import ProductTab from "./ProductTab.jsx";
-
+import Button from "./Button.jsx";
 
 function App() {
   return (
     <>
-      <h2>Blockbuster Deals | Shop Now</h2>
-
-      <ProductTab />
+      <Button/>
     </>
   );
 
