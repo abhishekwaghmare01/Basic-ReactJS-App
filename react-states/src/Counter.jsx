de9fc9a64ebf5fff26a02d361d6ec855ae3c41ba
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-01-14 16:19:25 +0530

    callback function added

--- react-states/src/Counter.jsx
+++ react-states/src/Counter.jsx
@@ -3,9 +3,13 @@ export default function Counter(){
     let [count,setCount] = useState(0);
 
     let incCount = () => {
-        count = count+1;
-        setCount(count);
-        // console.log(count);
+        // count = count+1;
+        setCount((currCount) => {
+            return currCount + 1;
+        });
+        setCount((currCount) => {
+            return currCount + 1;
+        });
         
     }
 
