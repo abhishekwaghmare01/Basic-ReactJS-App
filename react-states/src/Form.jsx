10cf7fbab8312d96cbe71746847c80968b7a24a6
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-02-17 23:27:58 +0530

    Update Form.jsx

--- react-states/src/Form.jsx
+++ react-states/src/Form.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export default function Form() {
+  let [fullName, setFullName] = useState("");
+
+  let handleChange = (event) =>{
+    event.preventDefault();
+    
+  }
+
   return (
     <form action="">
-      <input type="text" placeholder="enter your full name" />
+      <input type="text" placeholder="enter your full name" value={fullName} onChange={handleChange}/>
+      <button>Submit</button>
     </form>
   );
 }
